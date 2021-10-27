1df896c52c6077a24ffe1e689193ff4cc9d361a0
Tommaso Andreani <tAndreani@users.noreply.github.com>
2021-10-27 11:28:20 +0200

    Create simulateBCRs_SHMs_IGL.r

---
+++ Simulated_Data/simulateBCRs_SHMs_IGL.r
@@ -0,0 +1,43 @@
+R
+library(immuneSIM)
+
+# first obtain BCR fasta sequences with different amount of mutations in the variable domains (15, 30, 45, 60).
+# here an example for IGH BCR with 15 somatic hypermutations (SHM):
+
+sim_repertoire_IGL_shm15 <- immuneSIM(number_of_seqs = 100,
+                                        species = "hs",
+                                        receptor = "ig",
+                                        chain = "l",
+                                        shm.mode="data",
+                                        shm.prob=15/350)
+                     
+write.table(sim_repertoire_IGL_shm15,"igl_shm15.tsv",quote=F,col.names=T,row.names=F,sep="\t")
+
+
+sim_repertoire_IGL_shm30 <- immuneSIM(number_of_seqs = 100,
+                                        species = "hs",
+                                        receptor = "ig",
+                                        chain = "l",
+                                        shm.mode="data",
+                                        shm.prob=30/350)
+                     
+write.table(mm_igl_30_mutations,"igh_shm30.tsv",quote=F,col.names=T,row.names=F,sep="\t")
+
+sim_repertoire_IGL_shm45 <- immuneSIM(number_of_seqs = 100,
+                                        species = "hs",
+                                        receptor = "ig",
+                                        chain = "l",
+                                        shm.mode="data",
+                                        shm.prob=45/350)
+                     
+write.table(mm_igh_15_mutations,"igl_shm45.tsv",quote=F,col.names=T,row.names=F,sep="\t")
+
+sim_repertoire_IGL_shm60 <- immuneSIM(number_of_seqs = 100,
+                                        species = "hs",
+                                        receptor = "ig",
+                                        chain = "l",
+                                        shm.mode="data",
+                                        shm.prob=60/350)
+                     
+write.table(sim_repertoire_IGL_shm60,"igl_shm60.tsv",quote=F,col.names=T,row.names=F,sep="\t")
+q()
