d595244647f85949097a55cd20a1d1e90620e861
Tommaso Andreani <tAndreani@users.noreply.github.com>
2022-01-18 09:51:18 +0100

    Update Plot_Time.r

--- Scripts/Plot_Time.r
+++ Scripts/Plot_Time.r
@@ -1,11 +1,11 @@
 setwd("/cloud-data/snf-mgln-dds/AIDA/Bioinformatics/i0439277/RTcure/Benchmark/Pilot1_Sanger_TT/plot_time_execution/")
 library(ggplot2)
 library(tidyr)
-#install.packages("plyr")
 library(plyr)
 library(ggplot2)
-
+library(scales)
 time_exec <- read.table("Time_Execution_Methods_TT_plus_Linda.txt",header=T)
+head(time_exec)
 #names(time_exec)[8] <- "cov1mln.250k"
 long_df <- time_exec
 #long_df <- time_exec %>%  gather(coverage, time, cov50k:cov1mln.250k)
@@ -13,7 +13,7 @@ long_df <- time_exec
 
 head(long_df)
 long_df$Time <- as.numeric(long_df$Time)
-long_df$Coverage <- factor(long_df$Coverage, levels=c("50k", "100k", "250k", "500k", "750k", "1mln", "1m250k"))
+long_df$Coverage <- factor(long_df$Coverage, levels=c("50K", "100K", "250K", "500K", "750K", "1M", "1M250K"))
 long_df$Read_Length <- factor(long_df$Read_Length, levels=c("25bp", "50bp", "75bp", "100bp"))
 #now lets plot using the standatd deviation
 
@@ -32,6 +32,7 @@ data_summary <- function(data, varname, groupnames){
 }
 
 #2) compute the standard deviation
+long_df <- as.data.frame(long_df)
 df2 <- data_summary(long_df, varname="Time", 
                     groupnames=c("Coverage", "Method","Read_Length"))
 #quality check
@@ -41,52 +42,20 @@ head(df2)
 p <- ggplot(df2,aes(Coverage,Time,colour=Method,group=Read_Length))
 pd<-position_dodge(.9)
 
-p + 
-  geom_errorbar(aes(ymin=Time-sd,ymax=Time+sd),width=.1,position=pd,colour="black") +
+p +   geom_errorbar(aes(ymin=Time-sd,ymax=Time+sd),width=.1,position=pd,colour="black") +
   geom_point(position=pd,size=4) + geom_line(position=pd) + 
   theme_bw() +  
   facet_grid(Method~Read_Length)+theme(axis.text.x=element_text(angle = 90, vjust = 0.5)) +
   scale_y_continuous(trans = log2_trans(),breaks = trans_breaks("log2", function(x) 2^x),labels = trans_format("log2", math_format(2^.x)))+
   scale_y_log10()+
   annotation_logticks()+
-  ggtitle("Time of execution \n different coverage and reads length internal dataset")+ theme(plot.title = element_text(hjust = 0.5))+theme(text=element_text(size=15))+ xlab("Coverage") + ylab("Time (Seconds)")
+  ggtitle("")+ theme(plot.title = element_text(hjust = 0.5))+theme(text=element_text(size=15))+ xlab("Coverage") + ylab("Time (Seconds)")+
+  theme(text=element_text(size=15))+theme(axis.text.x = element_text(face="bold", color="#000001")) + theme(axis.text.y = element_text(face="bold", color="#000001"))+theme(text = element_text(size = 18, face="bold"))+theme(axis.title.y = element_text(size = 18))+theme(legend.text = element_text(size = 18,face="plain"))+theme(legend.title = element_text(size = 18))  
 
 
+#p+axis.title=element_text(size=18,face="bold")+theme(plot.title = element_text(hjust = 0.5))+theme(axis.text=element_text(size=14),axis.title=element_text(size=14,face="bold"))+ggtitle("")+ xlab("") + ylab("Time (Seconds)") +
 
 
-
-
-
-
-
-########################################################
-########################################################
-rm(list = ls())
-library(data.table)
-library(reshape2)
-
-time_exec <- read.table("Time_Execution_Methods_TT_plus_Linda.txt",header=T)
-#names(time_exec)[8] <- "cov1mln.250k"
-long_df <- time_exec
-#long_df <- time_exec %>%  gather(coverage, time, cov50k:cov1mln.250k)
-#long_df <- time_exec %>%  gather(coverage, time, Method)
-
-head(long_df)
-long_df$Time <- as.numeric(long_df$Time)
-long_df$Coverage <- factor(long_df$Coverage, levels=c("50k", "100k", "250k", "500k", "750k", "1mln", "1m250k"))
-long_df$Read_Length <- factor(long_df$Read_Length, levels=c("25bp", "50bp", "75bp", "100bp"))
-
-long_df
-#long_df <- subset(long_df, Coverage == "1m250k")
-long_df <- subset(long_df, Read_Length == "25bp")
-p <- ggplot(data=long_df,
-       aes(x=Coverage, y=Time, colour=Method)) +
-        geom_boxplot()
-
-p + scale_y_continuous(trans = log2_trans(),
-                       breaks = trans_breaks("log2", function(x) 2^x),
-                       labels = trans_format("log2", math_format(2^.x)))+
-  ggtitle("Time of execution \n different coverage 25bp internal dataset")+ theme(plot.title = element_text(hjust = 0.5))+theme(text=element_text(size=15))+ xlab("Coverage") + ylab("Time (Seconds)")
 
 
 
