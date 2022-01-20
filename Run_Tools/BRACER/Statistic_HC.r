46e75e502497a1d1db964e00e6d1884db5416414
Tommaso Andreani <tAndreani@users.noreply.github.com>
2022-01-20 14:54:20 +0100

    Update Statistic_HC.r

--- Run_Tools/BRACER/Statistic_HC.r
+++ Run_Tools/BRACER/Statistic_HC.r
@@ -4,54 +4,46 @@ library(tidyr)
 library(stringr)
 setwd("/root/cloud-data/snf-mgln-dds/AIDA/Bioinformatics/i0439277/RTcure/Benchmark/Baldr_data/ground_truth/fasta/")
 
-#Load Ground Truth data Heavy Chain
-HC <- read.table("heavy.Id.sra.sequence_igblast_db-pass.tsv",header=T,sep="\t")
-dim(HC)
-head(HC)
+#Load Ground Truth data Heavy Chain (from e.g. Leiden, Canzar or Updhyay dataset)
+HC <- read.table("HC.sequence_igblast_db-pass.tsv",header=T,sep="\t")
 
-#Exclude single end and low coverage samples which were not run
-
+#Extract productivitiy and genes' name
 Ground_Truth <- subset(HC, select = c(1,4,5,6,7))
-head(Ground_Truth)
-dim(Ground_Truth)
 tot_chains_productive <- Ground_Truth[ which(Ground_Truth$productive == "TRUE" ),]
-length(tot_chains_productive$sequence_id)
 percentage_productive <- length(tot_chains_productive$sequence_id)/length(Ground_Truth$sequence_id)
 print(percentage_productive)
+
+#rename the first column to extract id
 Ground_Truth_split <- Ground_Truth %>% separate(sequence_id, c("A","B","C"))
 Ground_Truth_split_subset <- subset(Ground_Truth_split, select = c(1,4,5,6,7))
 head(Ground_Truth_split_subset)
 names(Ground_Truth_split_subset)[names(Ground_Truth_split_subset) == "A"] <- "sequence_id"
 
 
-
-#Load Baldr Data
+#Load Data (e.g. from BALDR, LEIDEN or Canzar dataset reconstructed Heavy Chains samples)
 #coverage 100 and 1mln.250k
 
-setwd("/root/cloud-data/snf-mgln-dds/AIDA/Bioinformatics/i0439277/RTcure/Benchmark/Baldr_data/bracer/transfer/paired_end/75bp/run_50cov/test/tsv_files/IGH/")
-dataset_LC_L002 <- read.table("IGH_header.tsv",sep="\t",header=T,skipNul = T)
-head(dataset_LC_L002)
-###count number of Light Chain L and how many are productive
+setwd("path/to/files")
+dataset_LC_L002 <- read.table("IGH.tsv",sep="\t",header=T,skipNul = T)
+
+###count number of Heavy Chain and how many are productive
 dataset_LC_L002_productive <- subset(dataset_LC_L002, productive == "TRUE")
 print(paste0("Number Heavy Chain reconstructed   ",length(dataset_LC_L002$sequence_id)))
 print(paste0("Number Heavy Chain reconstructed productive   ",length(dataset_LC_L002_productive$sequence_id)))
 
 ##Parse the name and subset the chains that are both in ground truth and assembled
-LC_L002_BASIC_1mln_250L_productive_in_ground_truth <- dataset_LC_L002[ grepl(paste(Ground_Truth_split_subset$sequence_id, collapse="|"), dataset_LC_L002$sequence_id),]
-dim(dataset_LC_L002)
-dim(Ground_Truth_split_subset)
-dim(LC_L002_BASIC_1mln_250L_productive_in_ground_truth)
-head(LC_L002_BASIC_1mln_250L_productive_in_ground_truth)
-head(Ground_Truth_split_subset)
+BCR_productive_in_ground_truth <- dataset_LC_L002[ grepl(paste(Ground_Truth_split_subset$sequence_id, collapse="|"), dataset_LC_L002$sequence_id),]
 
 
-#Join Table basic 1mlnk 100bp
-joined_tables_all_basic <- full_join(LC_L002_BASIC_1mln_250L_productive_in_ground_truth,Ground_Truth_split_subset,by = c("sequence_id"),copy = TRUE)
+#Join Tables pf Ground truth and Reconstructed BCR
+joined_tables_all_basic <- full_join(BCR_productive_in_ground_truth,Ground_Truth_split_subset,by = c("sequence_id"),copy = TRUE)
 
 joined_tables_all_basic_in_ground_truth <- subset(joined_tables_all_basic, joined_tables_all_basic$productive.y == "TRUE" | joined_tables_all_basic$productive.y == "FALSE")
 dim(joined_tables_all_basic_in_ground_truth)
 head(joined_tables_all_basic_in_ground_truth)
-#Perform the match
+
+#Perform the match on v-d-j genes (i.e. check if at least one of each of the V-D-J genes of a productive HC in the ground truth matched one of each of the corresponding V-D-J genes obtained by the computational method
+
 #Match V genes
 v_genes <- joined_tables_all_basic_in_ground_truth %>% mutate(id = joined_tables_all_basic_in_ground_truth$sequence_id) %>% 
   separate_rows(v_call.x, sep = ',') %>%
@@ -81,12 +73,6 @@ v_genes$v_call.y <- replace(v_genes$v_call.y, is.na(v_genes$v_call.y), FALSE)
 names(v_genes)[10] <- "v.gene.match"
 v_genes
 
-
-
-####################################################################
-
-
-#Perform the match considering multiple genes in the output of the tool or the sanger for every V-D-J
 #Match D genes
 d_genes <- joined_tables_all_basic_in_ground_truth %>% mutate(id = joined_tables_all_basic_in_ground_truth$sequence_id) %>% 
   separate_rows(d_call.x, sep = ',') %>%
@@ -115,7 +101,7 @@ d_genes$v_call.y <- replace(d_genes$v_call.y, is.na(d_genes$v_call.y), FALSE)
 
 names(d_genes)[10] <- "d.gene.match"
 d_genes
-###################################################################################
+
 #Match J genes
 
 j_genes <- joined_tables_all_basic_in_ground_truth %>% mutate(id = joined_tables_all_basic_in_ground_truth$sequence_id) %>% 
@@ -148,32 +134,37 @@ names(j_genes)[10] <- "j.gene.match"
 j_genes
 
 
-#Match productivity
+#Collect all the match and compute the sensitivtiy 
 df <- as.data.frame(cbind(joined_tables_all_basic_in_ground_truth$sequence_id,v_genes$v.gene.match,d_genes$d.gene.match,v_genes$v.gene.match))
 head(df)
 #select only the chains that were productive in the ground truth
 df_productive <- df[df$V1 %in% Ground_Truth_split_subset$sequence_id,]
 dim(df_productive)
+
+#Create the table to compute the sensitivtiy
 Id <- df_productive$V1
 df_productive$V1 <- as.character(df_productive$V1) 
 df_productive$V2 <- as.logical(df_productive$V2)
 df_productive$V3 <- as.logical(df_productive$V3)
 df_productive$V4 <- as.logical(df_productive$V4)
+
+#Convert the mathc in 0 or 1
 df_productive <- 1*df_productive[,2:4]
-dim(df_productive)
+
+#Compute sensitivtiy 
 df_productive$sum <- rowSums(df_productive[,1:3])/3
-Accuracy <- cbind(Id,df_productive)
-dim(Accuracy)
-Alll_Match <- subset(Accuracy, sum == 1)
+Sensitivity <- cbind(Id,df_productive)
+dim(Sensitivity)
+Alll_Match <- subset(Sensitivity, sum == 1)
 value_all_match <- length(Alll_Match$Id)
-Accuaracy_Value <- value_all_match/length(Accuracy$Id)
-print(Accuaracy_Value)
+Sensitivity_value <- value_all_match/length(Sensitivity$Id)
+print(Sensitivity_value)
 print(value_all_match)
-print(length(Accuracy$Id))
+print(length(Sensitivity$Id))
 
 
 print(paste0("Number Heavy Chain reconstructed   ",length(dataset_LC_L002$sequence_id)))
 print(paste0("Number Heavy Chain reconstructed productive   ",length(dataset_LC_L002_productive$sequence_id)))
-print(paste0("Accuracy  ", value_all_match/length(Accuracy$Id)))
+print(paste0("Sensitivity  ", value_all_match/length(Sensitivity$Id)))
 
 
