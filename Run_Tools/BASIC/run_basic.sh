5358a850d8f65fce53e124c0b6431bacbe89b1f9
Tommaso Andreani <tAndreani@users.noreply.github.com>
2022-01-20 15:10:59 +0100

    Update run_basic.sh

--- Run_Tools/BASIC/run_basic.sh
+++ Run_Tools/BASIC/run_basic.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+#SBATCH --job-name=BCR_Reconstruction      # job name
+#SBATCH --array=1-48                       # Number of Job (according to the number of splitted files in the exported variables)
+#SBATCH --nodes=2                          # nodes
+#SBATCH -c 2                               # cores
+#SBATCH --mem=6G                           # memory
+#SBATCH --time=72:00:00                    # time
+#SBATCH --error=BCR_TCR.err                # error file name
+#SBATCH --output=BCR_TCR.out               # output file name
+#SBATCH --mail-user=Tommaso.Andreani@sanofi.com  # email
+#SBATCH --mail-type=ALL                          # type notification
+
 files="/site/ne/home/i0439277/TRUST4/Pilot1/mapping2"
 output="/site/ne/home/i0439277/basic/benchmark/pilot1_TT_plus_Linda"
 
