2283eaaad37e769eb4f1fd31f395c13ae9a673b3
Tommaso Andreani <tAndreani@users.noreply.github.com>
2022-01-20 15:47:19 +0100

    Update run_basic.sh

--- Run_Tools/BASIC/run_basic.sh
+++ Run_Tools/BASIC/run_basic.sh
@@ -10,6 +10,9 @@
 #SBATCH --mail-user=Tommaso.Andreani@sanofi.com  # email
 #SBATCH --mail-type=ALL                          # type notification
 
+export fastq=`sed -n "$SGE_TASK_ID"p files.txt`
+
+
 files="/path/input_fastq/"
 output="/path/output/"
 
